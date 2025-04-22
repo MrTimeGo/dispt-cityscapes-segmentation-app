014b7c7089fed1a92a0126293e57c258fdd7c579
Artem Petselia <mrtimego@ukr.net>
2025-04-23 00:27:32 +0300

    Init frontend (#1)
    * Add .gitignore file to exclude unnecessary files and directories
    
    * Initialize Angular frontend application with essential configuration files and structure
    
    * Integrate Tailwind CSS into Angular application by adding PostCSS configuration, updating stylesheets, and modifying package dependencies. Replace SCSS with CSS for global styles.
    
    * Add PrimeNG components and themes to Angular application, update package dependencies, and replace placeholder content in app.component.html with PrimeNG buttons.
    
    * Add ESLint and Prettier configuration files, update package dependencies for linting and formatting tools, and enable automatic formatting on save in VSCode settings.

---
+++ cs-seg-frontend-app/eslint.config.mjs
@@ -0,0 +1,49 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import css from '@eslint/css';
+import { defineConfig } from 'eslint/config';
+import tsparser from '@typescript-eslint/parser';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  {
+    files: ['**/*.css'],
+    plugins: { css },
+    language: 'css/css',
+    extends: ['css/recommended'],
+  },
+  {
+    files: ['**/*.ts'],
+
+    languageOptions: {
+      parser: tsparser,
+      sourceType: 'module',
+    },
+
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin,
+    },
+
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      ...prettierConfig.rules,
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'no-console': 'warn',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      'prettier/prettier': 'error',
+    },
+  },
+]);
